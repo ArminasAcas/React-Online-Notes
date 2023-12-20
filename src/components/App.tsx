9f5463b32a08f3cd09c8e16c7cd9051a2b32ba58
ArminasAcas <arminasprollas@gmail.com>
2023-12-20 22:09:36 +0200

    Added React routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,19 @@ import '../css/App.css'
 import Home from '../pages/HomePage'
 import Login from '../pages/LoginPage'
 import Register from '../pages/RegisterPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function App() {
 
   return (
-    <>
-      <Register/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/Home" replace />}/> 
+        <Route index path='/Home' element={<Home/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
