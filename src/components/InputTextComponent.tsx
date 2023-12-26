919e6814d02595ceb5a61fc011ab3c132d1938be
ArminasAcas <arminasprollas@gmail.com>
2023-12-26 21:04:21 +0200

    Improved InputText component by adding extra props

--- src/components/InputTextComponent.tsx
+++ src/components/InputTextComponent.tsx
@@ -1,8 +1,28 @@
 import "../css/InputTextComponent.css"
 
-export default function InputText(props: {id: string, name: string, type:string}) {
+interface InputTextProps {
+    id: string,
+    name: string,
+    type: string,
+    value: string,
+    onChange: (e : React.ChangeEvent<HTMLInputElement>) => void;
+    isRequired ?: boolean;
+    minLenght ?: number;
+    maxLenght ?: number;
+};
+
+export default function InputText(props: InputTextProps) {
 
     return (
-        <input className="input-text" type={props.type} id={props.id} name={props.name}></input>
+        <input 
+            className="input-text" 
+            type={props.type} 
+            id={props.id} name={props.name} 
+            value={props.value} 
+            onChange={props.onChange} 
+            required={props.isRequired ? true : undefined}
+            minLength={props.minLenght ? props.minLenght : undefined}
+            maxLength={props.maxLenght ? props.maxLenght : undefined}>
+        </input>
     )
 }
