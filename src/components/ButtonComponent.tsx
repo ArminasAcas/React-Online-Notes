1d6f096a6f5edd578b4b0d8366d7f666ab1b0376
ArminasAcas <arminasprollas@gmail.com>
2024-02-05 23:36:17 +0200

    button component improvements

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,5 +1,7 @@
 import "../css/ButtonComponent.css"
 
-export default function Button(props: {text:string, onClick?: () => void}) {
-    return <button type="button" onClick={props.onClick} className="button">{props.text}</button>
+export default function Button(props: {text:string, onClick?: () => void, onClickKeepPressed?: boolean}) {
+    let classNames = "button";
+    if (props.onClickKeepPressed) classNames += " button--pressed"
+    return <button type="button" onClick={props.onClick} className={classNames}>{props.text}</button>
 }
